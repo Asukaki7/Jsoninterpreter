524aa85cfb667fadeb87af5a5c550501f397926b
root <root@Asukaki>
2024-03-11 12:18:56 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@
 #include <optional>
 #include <array>
 #include <functional>
+#include "print.h"
+
+
+struct JSONObject;
+
+using JSONDict = std::unordered_map<std::string, JSONObject>;
+using JSONList = std::vector<JSONObject>;
+
+
 struct JSONObject {
    std::variant
     < std::monostate // null
@@ -19,6 +28,26 @@ struct JSONObject {
     , std::vector<JSONObject>  //[42, "hello"]
     , std::unordered_map<std::string, JSONObject> //{"hello":985, "world":211}
     > inner;
+
+    void do_print() const {
+
+    }
+
+    template <class T>
+    bool is() const {
+        return std::holds_alternative<T>(inner);
+    }
+    
+    template <class T>
+    T const &get() const {
+        return std::get<T>(inner);
+    }
+
+    template <class T>
+    T &get() {
+        return std::get<T>(inner);
+    }
+     
 }; 
 
 
@@ -32,34 +61,124 @@ std::optional<T> try_parse_num(std::string_view str) {
     return std::nullopt;
 }
 
-JSONObject parse(std::string_view json) {
-    if (json.empty()) {
-        return JSONObject{std::nullptr_t{}};
+char unescaped_char(char c) {
+    switch (c) {
+        case 'n' : return '\n';
+        case 'r' : return '\r';
+        case '0' : return '\0';
+        case 't' : return '\t';
+        case 'v' : return '\v';
+        case 'f' : return '\f';
+        case 'b' : return '\b';
+        case 'a' : return '\a';
+        default : return c;
     }
-    if (json[0] >= '0' && json[0] <= '9' || json[0] == '-' || json[0] == '+') {
-        std::regex num_re{"[+-]?[0-9]+(\\.[0-9]*)>([eE][+-]?[0-9]+)?"};
+}
+
+
+std::pair<JSONObject, size_t> parse(std::string_view json) {
+    if(json.empty()) {
+        return {JSONObject{std::nullptr_t{}}, 0};
+    } else if (size_t off = json.find_first_not_of(" \n\r\t\v\f\0"); off != 0 && off != json.npos){
+        auto [obj, eaten] = parse(json.substr(off));
+        return {std::move(obj), eaten + off};
+    }
+    else if (json[0] >= '0' && json[0] <= '9' || json[0] == '-' || json[0] == '+') {
+        std::regex num_re{"[+-]?[0-9]+(\\.[0-9]*)?([eE][+-]?[0-9]+)?"};
         std::cmatch match;
         if (std::regex_search(json.data(), json.data() + json.size(), match, num_re)) {
             std::string str = match.str();
             if (auto num = try_parse_num<int>(str); num.has_value()) {
-                return JSONObject{*num};
+                return {JSONObject{*num}, str.size()};
             }
             if (auto num = try_parse_num<double>(str);num.has_value()) {
-                return JSONObject{*num};
+                return {JSONObject{*num}, str.size()};
             }
         }
-    }
-
-    if ('"' == json[0]) {
-        std::string str;
+    } else if ('"' == json[0]) {
+        std::string str{};
         enum {
             Raw,
             Escaped,
         } phase =  Raw;
+        size_t i;
+        for(i = 1; i < json.size(); i++) {
+            char ch = json[i];
+            if (phase == Raw) {
+                if (ch == '\\') {
+                    phase = Escaped;
+                } else if (ch == '"') {
+                    i += 1;
+                    break;
+                } else {
+                    str += ch;
+                }
+            } else if (phase == Escaped) {
+                str += unescaped_char(ch);
+                phase = Raw;
+            } 
+        }
+        return {JSONObject {std::move(str)}, i};
+    } else if (json[0] == '[') {
+        std::vector<JSONObject> res;
+        size_t i;
+        for(i = 1; i < json.size();) {
+            if(json[i] == ']') {
+                i += 1;
+                break;
+            }
+            auto [obj, eaten] = parse(json.substr(i));
+            if(eaten == 0) {
+                i == 0;
+                break;
+            }
+            res.push_back(std::move(obj));
+            i += eaten;
+            if (json[i] == ',') {
+                i += 1;
+            }
+        }
+        return {JSONObject{std::move(res)}, i};
         
+    } else if (json[0] == '{') {
+        std::unordered_map<std::string, JSONObject> res;
+        size_t i;
+        for(i = 1; i < json.size();) {
+            if (json[i] == '}') {
+                i += 1;
+                break;
+            }
+            auto [keyobj, keyeaten] = parse(json.substr(i));
+            if (keyeaten == 0) {
+                i = 0;
+                break;
+            }
+            i += keyeaten;
+            if (!std::holds_alternative<std::string>(keyobj.inner)) {
+                i = 0;
+                break;
+            }
+            if (json[i] == ':') {
+                i += 1;
+            }
+            std::string key = std::move(std::get<std::string>(keyobj.inner));
+            auto [valobj, valeaten] = parse(json.substr(i));
+            if(valeaten == 0) {
+                i = 0;
+                break;
+            }
+            i += valeaten;
+            res.insert_or_assign(std::move(key), std::move(valobj));
+            //= res[key] = valobj; 更高效
+            if (json[i] == ',') {
+                i += 1;
+            }
+        }
     }
+   
+        
 
-    return JSONObject{std::nullptr_t{}};
+    return {JSONObject{std::nullptr_t{}}, 0};
 }
 
 struct PrintNum {
@@ -69,15 +188,11 @@ struct PrintNum {
     }
 };
 
-std::function<void(int)> f = PrintNum();
-
-std::function<std::array<int, 2>(int, int)> solve = [&](int s,int x) -> std::array<int, 2>{};
-
-auto a = [&](int s,int x) {
-    std::array<int, 2> a;
-    return a;
-};
 
 int main() {
-    
+    std::string_view str3 = R"JSON([1,2,3])JSON";
+    auto [obj, eaten] = parse(str3);
+    print(obj);
+    /* std::cout << std::get<std::string>(parse(str3).first.inner); */
+    return 0;
 } 
